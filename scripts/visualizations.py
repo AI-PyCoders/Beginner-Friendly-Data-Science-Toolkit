d0d1dd1f0e15747a84f98b66aa9a1c8d2f61b5b4
Neelesh Vashist <neeleshvashist@gmail.com>
2024-10-27 15:09:34 -0400

    Add: iris notebook

--- scripts/visualizations.py
+++ scripts/visualizations.py
@@ -7,14 +7,15 @@ def plot_histogram(df, column):
     plt.title(f'Histogram of {column}')
     plt.xlabel(column)
     plt.ylabel('Frequency')
+    plt.grid(axis='y', alpha=0.75)
     plt.show()
 
 
-def plot_scatter(df, x_column, y_column):
+def plot_scatter(df, x, y):
     plt.figure(figsize=(8, 6))
-    plt.scatter(df[x_column], df[y_column],
-                color='blue', edgecolor='k', alpha=0.7)
-    plt.title(f'Scatter Plot of {x_column} vs {y_column}')
-    plt.xlabel(x_column)
-    plt.ylabel(y_column)
+    plt.scatter(df[x], df[y], alpha=0.7)
+    plt.title(f'Scatter Plot of {y} vs {x}')
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.grid()
     plt.show()
